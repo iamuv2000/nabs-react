14c16ed701690a22256b030ded532a3851faf492
iamuv2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-05-12 10:44:22 +0530

    Integrated final login page

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './LoginPage.css';
 import NABS from '../../assets/nabs.png';
+import { Button } from '@material-ui/core';
+import googleIcon from '../../assets/icons8-google.svg';
+
 
 class Signup extends React.Component{
     render(){
@@ -11,12 +14,20 @@ class Signup extends React.Component{
                 </div>
                 <div id="login_page_content">
                     <div id="login_page_form">
-                        <h1>Sign up</h1>
-                        <input type = "email" placeholder="Email"/>
-                        <input type = "password" placeholder="Password"/>
+                        <h1 id="signup_heading">Log in</h1>
+                        <input type = "email" placeholder="Email" className="signup_input"/>
+                        <input type = "password" placeholder="Password" className="signup_input"/>
                         <br />
-                        <a href="#">Forgot password?</a>
-                        <button>Login</button>
+                        <div id="signup_forgot_password_container">
+                            <a href="#" id="signup_forgot_password">Forgot password?</a>
+                        </div>
+                        <br />
+                        <br />
+                        <button id="signup_page_signup_button">Login</button>
+                        <Button variant="outlined" className="googleButton" style={{borderWidth:2, borderColor:"black", borderRadius:5}}>
+                            <img src={googleIcon} alt="gicon" className="gicon" height="24" width="24"/>
+                            Continue with Google
+                        </Button> 
                         <div id="auth_or">
                             <hr class="line"></hr>
                             <strong>OR</strong>
@@ -24,7 +35,7 @@ class Signup extends React.Component{
                         </div>   
                         <div id="auth_options_container">
                             <a href="/" className="auth_options">Home</a>
-                            <strong>OR</strong>
+                            <strong>|</strong>
                             <a href="/signup" className="auth_options">Sign Up</a>
                     </div>          
                     </div>
