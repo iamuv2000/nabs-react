f68596c40876dd72d0da027c7771347ca629649f
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2020-10-12 00:17:52 +0530

    Routing changes for Dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LandingPage from './Pages/LandingPage/LandingPage';
 import Signup from './Pages/Signup/Signup';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import Hack from './Pages/Hack/Hack';
+import Dashboard from './Pages/Dashboard/Dashboard';
+
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
         <Route path='/' component={LandingPage} exact={true}  />
         <Route path='/signup' component={Signup} exact={true}  />
         <Route path='/login' component={LoginPage} exact={true}  />
-        <Route path='/dashboard' component={Hack} exact={true}  />
+        <Route path='/dashboard' component={Dashboard} exact={true}  />
       </Switch>
     </BrowserRouter>
     </div>
